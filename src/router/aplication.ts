8c03650303fc49d12fda635e8d737c7fa687330b
RamonUlises <jorgitomilina@gmail.com>
2025-01-27 14:56:44 -0600

    Actualizar versión de la aplicación a 1.1.1 y modificar enlaces de descarga

--- src/router/aplication.ts
+++ src/router/aplication.ts
@@ -4,15 +4,15 @@ import path from 'node:path';
 const router = Router();
 
 const latestVersion = {
-  version: '1.1.0',
+  version: '1.1.1',
   notes: 'Nueva actualización',
   pub_date: '2025-01-22T12:00:00Z',
   platforms: {
     windows: {
-      url: 'https://aricab.nextstep-web.online/aplicacion/actualizar/app-1.1.0.exe',
+      url: 'https://aricab.nextstep-web.online/aplicacion/actualizar/aricab_1.1.1_x64-setup.exe',
     },
     linux: {
-      url: 'https://aricab.nextstep-web.online/aplicacion/actualizar/app-1.1.0.deb',
+      url: 'https://aricab.nextstep-web.online/aplicacion/actualizar/aricab_1.1.1_amd64.deb',
     }
   }
 };
