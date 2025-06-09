229f090e7441923ba957bd2efcb6fe33d0dedbc1
RamonUlises <jorgitomilina@gmail.com>
2025-06-09 05:01:30 -0600

    Refactorizar métodos en el modelo de facturas para mejorar la legibilidad y la estructura del código

--- src/models/facturas.ts
+++ src/models/facturas.ts
@@ -9,11 +9,11 @@ class FacturasModels {
   async obtenerFacturas(fecha: string) {
     try {
       const localDate = new Date(fecha); // Este ya está en UTC-6 por el string que envías
-  
+
       // Calculamos el inicio y fin del día en UTC-6, pero convertimos al UTC real que maneja el servidor
       const inicioDelDia = new Date(localDate);
       inicioDelDia.setUTCHours(6, 0, 0, 0); // UTC-6 => 00:00 en local es 06:00 en UTC
-  
+
       const finDelDia = new Date(localDate);
       finDelDia.setUTCHours(29, 59, 59, 999); // 23:59 en UTC-6 es 05:59 del día siguiente en UTC
 
@@ -54,17 +54,34 @@ class FacturasModels {
       );
 
       await FacturasSchemas.create({ ...factura, total: Math.ceil(suma) });
-      await UsuarioModels.actualizarCantidad(factura['id-facturador'], factura.productos);
+      await UsuarioModels.actualizarCantidad(
+        factura['id-facturador'],
+        factura.productos,
+      );
 
-      io.emit('facturaAdd', { id: factura.id, nombre: factura.nombre, fecha: factura.fecha, productos: factura.productos, tipo: factura.tipo, total: Math.ceil(suma), pagado: factura.pagado, 'id-facturador': factura['id-facturador'] });
+      io.emit('facturaAdd', {
+        id: factura.id,
+        nombre: factura.nombre,
+        fecha: factura.fecha,
+        productos: factura.productos,
+        tipo: factura.tipo,
+        total: Math.ceil(suma),
+        pagado: factura.pagado,
+        'id-facturador': factura['id-facturador'],
+      });
 
       return 'Factura creada';
-    } catch(err) {
+    } catch (err) {
       console.log(err);
       return 'Error al crear la factura';
     }
   }
-  async actualizarFactura(id: string, productos: ProductoFacturaType[], tipo: string, pagado: number) {
+  async actualizarFactura(
+    id: string,
+    productos: ProductoFacturaType[],
+    tipo: string,
+    pagado: number,
+  ) {
     try {
       const factur = await FacturasSchemas.findOne({ id });
 
@@ -79,10 +96,24 @@ class FacturasModels {
         0,
       );
 
-      await FacturasSchemas.updateOne({ id }, { productos, total: Math.ceil(suma), tipo, pagado });
-      await UsuarioModels.actualizarCantidadUpdate(factur['id-facturador'], productos, factur.productos);
+      await FacturasSchemas.updateOne(
+        { id },
+        { productos, total: Math.ceil(suma), tipo, pagado },
+      );
+      await UsuarioModels.actualizarCantidadUpdate(
+        factur['id-facturador'],
+        productos,
+        factur.productos,
+      );
 
-      io.emit('facturaUpdate', { id, productos, total: Math.ceil(suma), tipo, pagado, facturador: factur['id-facturador'] });
+      io.emit('facturaUpdate', {
+        id,
+        productos,
+        total: Math.ceil(suma),
+        tipo,
+        pagado,
+        facturador: factur['id-facturador'],
+      });
 
       return 'Factura actualizada';
     } catch {
@@ -98,7 +129,10 @@ class FacturasModels {
       }
 
       await FacturasSchemas.deleteOne({ id });
-      await UsuarioModels.actualizarCantidadDelete(facturador, factur.productos);
+      await UsuarioModels.actualizarCantidadDelete(
+        facturador,
+        factur.productos,
+      );
       io.emit('facturaDelete', id, factur['id-facturador']);
       io.emit('updateProd');
 
@@ -121,21 +155,21 @@ class FacturasModels {
       io.emit('facturaAbonar', { id, total });
 
       return 'Factura abonada';
-    }  catch {
+    } catch {
       return 'Error al abonar la factura';
     }
   }
   async obtenerFacturasFacturador(id: string, fecha: string) {
     try {
       const localDate = new Date(fecha); // Este ya está en UTC-6 por el string que envías
-  
+
       // Calculamos el inicio y fin del día en UTC-6, pero convertimos al UTC real que maneja el servidor
       const inicioDelDia = new Date(localDate);
       inicioDelDia.setUTCHours(6, 0, 0, 0); // UTC-6 => 00:00 en local es 06:00 en UTC
-  
+
       const finDelDia = new Date(localDate);
       finDelDia.setUTCHours(29, 59, 59, 999); // 23:59 en UTC-6 es 05:59 del día siguiente en UTC
-  
+
       const facturas: FacturaType[] = await FacturasSchemas.find({
         'id-facturador': id,
         fecha: {
@@ -143,17 +177,20 @@ class FacturasModels {
           $lte: finDelDia,
         },
       });
-  
+
       return facturas;
     } catch (error) {
       console.error('Error al obtener facturas:', error);
       return [];
     }
-  }  
-  
+  }
+
   async actualizarClienteFactura(lastName: string, newName: string) {
     try {
-      await FacturasSchemas.updateMany({ nombre: lastName }, { nombre: newName });
+      await FacturasSchemas.updateMany(
+        { nombre: lastName },
+        { nombre: newName },
+      );
 
       io.emit('updateName');
 
@@ -162,24 +199,36 @@ class FacturasModels {
       return 'Error al actualizar las facturas';
     }
   }
-  async obtenerCredito(): Promise<{ count: number, total: number }> {
+  async obtenerCredito(): Promise<{ count: number; total: number }> {
     try {
       const credito = await FacturasSchemas.find({ tipo: 'crédito' });
 
-      const total = credito.reduce((acc: number, factura: FacturaType) => acc + factura.total - factura.pagado, 0);
+      const total = credito.reduce(
+        (acc: number, factura: FacturaType) =>
+          acc + factura.total - factura.pagado,
+        0,
+      );
 
-      const creditoPagado = credito.reduce((acc: number, factura: FacturaType) => {
-        if(factura.pagado === factura.total) return acc;
+      const creditoPagado = credito.reduce(
+        (acc: number, factura: FacturaType) => {
+          if (factura.pagado === factura.total) return acc;
 
-        return acc + 1;
-      }, 0);
+          return acc + 1;
+        },
+        0,
+      );
 
       return { count: creditoPagado, total };
     } catch {
       return { count: 0, total: 0 };
     }
   }
-  async obtenerResumenSemanas(): Promise<{ facturas1: FacturaType[], facturas2: FacturaType[], fecha1: string, fecha2: string }> {
+  async obtenerResumenSemanas(): Promise<{
+    facturas1: FacturaType[];
+    facturas2: FacturaType[];
+    fecha1: string;
+    fecha2: string;
+  }> {
     try {
       const ahora = new Date();
       const lunesActual = getMondayUTCMinus6(ahora);
@@ -212,7 +261,28 @@ class FacturasModels {
         },
       });
 
-      return { facturas1, facturas2, fecha1: `${lunesSemanaPasada.toLocaleDateString()}-${domingoSemanaPasada.toLocaleDateString()}`, fecha2: `${lunesSemanaAnterior.toLocaleDateString()}-${domingoSemanaAnterior.toLocaleDateString()}` };
+      return {
+        facturas1,
+        facturas2,
+        fecha1: `${lunesSemanaPasada.toLocaleDateString('es-MX', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        })}-${domingoSemanaPasada.toLocaleDateString('es-MX', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        })}`,
+        fecha2: `${lunesSemanaAnterior.toLocaleDateString('es-MX', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        })}-${domingoSemanaAnterior.toLocaleDateString('es-MX', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        })}`,
+      };
     } catch {
       return { facturas1: [], facturas2: [], fecha1: '', fecha2: '' };
     }
